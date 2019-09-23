6b57fc67dca931af9cb8fa086dd1f8b0d5f17119
Marco Antonio Popovich <m.popovich20@gmail.com>
2019-09-23 19:27:07 -0300

    WIP

--- src/main/java/com/gymroutine/backend/entrypoint/DummyController.java
+++ src/main/java/com/gymroutine/backend/entrypoint/DummyController.java
@@ -1,13 +1,23 @@
 package com.gymroutine.backend.entrypoint;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("dummy")
+@Api(tags = "Dummy API", description = "This API has operations related to Dummy Controller")
 public class DummyController {
 
-    @RequestMapping("/hi")
+    @GetMapping("/hi")
+    @ApiOperation("Dummy Operations")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "OK"),
+        @ApiResponse(code = 400, message = "Bad Request") })
     public String index() {
         return "Greetings!";
     }
